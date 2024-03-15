9e7017a36ed87bf1b99167f44fbed9d6b3c195e2
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-15 15:24:55 +0100

    comments

--- js/script.js
+++ js/script.js
@@ -11,8 +11,10 @@ con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
 
+// creo una variabile button e seleziono il bottone dall'html tramite la classe
 let button = document.querySelector('btn');
 
+// aggiungo un ascoltatore al button, attraverso un click accadranno cose.
 button.addEventListener('click'){
-    
+
 }
