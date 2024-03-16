3605185630219668853bec152d5a8db36913dc79
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-16 16:24:00 +0100

    created a cycle for  and added to btnplay with a eventListener with inside a function to generate a square.
    created a btnreset with eventListener to wipe a grid

--- js/script.js
+++ js/script.js
@@ -10,34 +10,38 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
+
 // tmplate = <div class="square"></div>
 
-
-
 // creo una variabile btnplay e seleziono il bottone dall'html tramite l'id
-const btnplay = document.getElementById('play')
+const btnplay = document.getElementById("play");
+console.log(btnplay);
+
+const grid = document.getElementById("grid");
 
 // aggiungo un ascoltatore al button e attraverso un click accadranno cose.
-btnplay.addEventListener('click', function(){
-    
-})
+btnplay.addEventListener("click", function () {
+  for (let index = 0; index < 100; index++) {
+    grid.appendChild(generateNewGridSquare());
+  }
+});
 
+function generateNewGridSquare() {
+  // definisco una variabile non riassegnabile per creare un nuovo elemento nel DOM <div></div>
+  const newEl = document.createElement("div");
+
+  //aggiungo all'elemento la classe .square
+  newEl.classList.add("square");
+
+  // ritorno l'elemento fuori dalla funzione
+  return newEl;
+}
 
 // creo una variabile btnreset e seleziono il bottone dall'html tramite l'id
-const btnreset = document.getElementById('reset')
+const btnreset = document.getElementById("reset");
 
 // aggiungo un ascoltatore al btnreset e attraverso un click accadranno cose.
-btnreset.addEventListener('click', function(){
-
-})
-
-
-function generateGridSquare(){
-
-    // definisco una variabile non riassegnabile per creare un elemento <div></div>
-    const newEl = document.createElement('div');
-
-    //aggiungo all'elemento la classe square
-    newEl.classList.add('square');
-
-}
+btnreset.addEventListener("click", function () {
+    // rimuovo gli elementi contenuti nella griglia
+  grid.innerHTML = "";
+});
